0fd87f6f4e9409ace0a4c512c7f7becd1671209d
Jason Olmstead <jason.olmstead@nordstrom.com>
2016-12-14 15:12:09 -0800

    removing tokens

--- src/github.js
+++ src/github.js
@@ -1,73 +1,92 @@
 var rp = require('request-promise'),
-    HttpsProxyAgent = require('https-proxy-agent');;
+    HttpsProxyAgent = require('https-proxy-agent'),
+    Promise = require('bluebird');
 
 const BASE_URL = "https://api.github.com/repos/";
 const AUTH_URL = "https://github.com/login/oauth/authorize";
 const OWNER = "Nordstrom/";
 const REPO = "build-bot/";
-const TOKEN = "access_token=0fe2c88a900ed9d2e13eaeb375176ccb3d4636d8";
-const CLIENT_ID_AND_SECRET = "client_id=9536727b9f252ceb5e03&client_secret=8ebef8e5e15d50472b3656d70bd2cec5ad887e0f";
 const BASIC_AUTH = "Basic amFzb25vbG1zdGVhZDMzOkJAc2ViYWxsMzM=";
+const MERGE_MESSAGE = "Merging master into branch: ";
+const MASTER_MERGE_MESSAGE = "Merging into master from: ";
 
 var Github = {
-    commit : function(){
-        console.log("Committing...")
-    },
-    authorizeAsync : function(){
-        var params = {
-            method : "GET",
-            uri : AUTH_URL + "?client_id=" + CLIENT_ID
-        };
-
-        if (!!process.env.LOCAL){
-            params.agent = new HttpsProxyAgent("http://webproxysea.nordstrom.net:8181");
+    preCheck : function(branch) {
+        if (!branch){
+            return Promise.reject("Invalid request -- need branch name")
         }
 
-        return rp(params)
-            .then(function(data){
-                console.log(data);
-            })
-    },
-    mergeAsync : function(base, head, message){
-        if (!base || !head || !message){
-            return Promise.reject("Invalid request")
-        }
-
-        var params = {
-            uri : BASE_URL + OWNER + REPO + "merges",
-            method : "POST",
-            json : true,
-            headers : {
-                "Accept": "application/json",
-                "Content-Type": "application/json",
-                "User-Agent" : "Hackathon-Local-Dev",
-                "Authorization" : BASIC_AUTH
-            },
-            body : {
-                "base": base,
-                "head": head,
-                "commit_message": message
-            }
-        };
-
-        if (!!process.env.LOCAL){
-            params.agent = new HttpsProxyAgent("http://webproxysea.nordstrom.net:8181");
-        }
-
-        return rp(params)
-            .then(function(data){
-                console.log("DATA")
-                console.log(data);
+        return merge(branch, "master", MERGE_MESSAGE + branch)
+            .then(function(){
+                console.log("Success...");
             })
             .catch(function(err){
-                console.log("ERR")
-                console.log(err.message);
-                console.log(err.response.headers);
+                if (err.message.message.indexOf("409")){
+                    return Promise.reject("Conflict merging master into " + branch);
+                } else {
+                    return Promise.reject(err.message);
+                }
+            })
+    },
+
+    mergeToMaster : function(branch){
+        if (!branch){
+            return Promise.reject("Invalid request -- need branch name")
+        }
+
+        return merge("master", branch, MASTER_MERGE_MESSAGE + branch)
+            .then(function(){
+                console.log("Success...");
+            })
+            .catch(function(err){
+                if (err.message.message.indexOf("409")){
+                    return Promise.reject("Conflict merging " + branch + "into master");
+                } else {
+                    return Promise.reject(err.message);
+                }
             })
     }
 };
 
 module.exports = Github;
 
-//Github.authorizeAsync()
-Github.mergeAsync("test-bot-branch", "master", "First merge of bot branches")
+
+function merge(base, head, message){
+    if (!base || !head || !message){
+        return Promise.reject("Invalid request, cant merge")
+    }
+
+    var params = {
+        uri : BASE_URL + OWNER + REPO + "merges",
+        method : "POST",
+        json : true,
+        headers : {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "User-Agent" : "Hackathon-Local-Dev",
+            "Authorization" : BASIC_AUTH
+        },
+        body : {
+            "base": base,
+            "head": head,
+            "commit_message": message
+        }
+    };
+
+    params = checkProxy(params);
+
+    return rp(params);
+}
+
+function checkProxy(params){
+    if (!!process.env.LOCAL){
+        params.agent = new HttpsProxyAgent("http://webproxysea.nordstrom.net:8181");
+    }
+    return params;
+}
+
+
+
+
+//** TEST CODE ****/
+Github.mergeToMaster("test-bot-branch");
