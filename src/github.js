4e383f484235758b5db7a93974bc89fc13d4aca2
Jason Olmstead <jason.olmstead@nordstrom.com>
2016-12-14 15:43:33 -0800

    auto commit from bot: Testing skip ci

--- src/github.js
+++ src/github.js
@@ -9,7 +9,7 @@ const OWNER = "Nordstrom/";
 const REPO = "build-bot/";
 const BASIC_AUTH = "Basic amFzb25vbG1zdGVhZDMzOkJAc2ViYWxsMzM=";
 const MERGE_MESSAGE = "Merging master into branch: ";
-const MASTER_MERGE_MESSAGE = "Merging into master from: ";
+const MASTER_MERGE_MESSAGE = "[skip ci] Merging into master from: ";
 
 var Github = {
     push : function(branch, message){
@@ -48,21 +48,55 @@ var Github = {
             })
     },
 
-    mergeToMaster : function(branch){
-        if (!branch){
+    mergeToMaster : function(branch) {
+        if (!branch) {
             return Promise.reject("Invalid request -- need branch name")
         }
 
         return merge("master", branch, MASTER_MERGE_MESSAGE + branch)
-            .then(function(){
+            .then(function () {
                 console.log("Success...");
             })
-            .catch(function(err){
-                if (err.message.message.indexOf("409")){
+            .catch(function (err) {
+                if (err.message.message.indexOf("409")) {
                     return Promise.reject("Conflict merging " + branch + "into master");
                 } else {
                     return Promise.reject(err.message);
                 }
+            })
+    },
+
+    release : function(version, notes){
+        var params = {
+            uri : BASE_URL + OWNER + REPO + "releases",
+            method : "POST",
+            json : true,
+            headers : {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "User-Agent" : "Hackathon-Local-Dev",
+                "Authorization" : BASIC_AUTH
+            },
+            body : {
+                "tag_name": "v" + version,
+                "target_commitish": "master",
+                "name": "RELEASE - v"+version,
+                "body": notes,
+                "draft": false,
+                "prerelease": false
+            }
+        };
+
+        params = checkProxy(params);
+
+        return rp(params)
+            .then(function(data){
+                console.log("data");
+                console.log(data);
+            })
+            .catch(function(err){
+                console.log("err")
+                console.log(err);
             })
     }
 };
@@ -108,4 +142,5 @@ function checkProxy(params){
 
 
 //** TEST CODE ****/
-Github.push("test-bot-branch", "testing push from shelljs");
+Github.push('test-bot-branch', "Testing skip ci");
+// Github.release("0.0.1", "[skip ci]");
