793353fde00a299b7a9a91a8b3341c91bcb635d9
Jason Olmstead <jason.olmstead@nordstrom.com>
2016-12-14 16:24:36 -0800

    adding versioning call

--- src/versioning.js
+++ src/versioning.js
@@ -8,15 +8,15 @@ const MINOR = "minor";
 
 var Versioning = {
     update : function(type){
-        if (type != PATCH && type != MAJOR && type != MINOR){
+        if (type != PATCH && type != MAJOR && type != MINOR) {
             throw new Error("Not valid verstion update type");
         }
-        var result = sh.exec("npm version " + type + " --force");
-        console.log(result);
+        var result = sh.exec("npm version " + type + " --force", {silent : true});
         if (result.code == 1){
             throw new Error("Error updating version");
         } 
         var version = result.stdout.replace('\n', "").replace("v", "");
+        console.log("Updated to version: v" + version);
         return version;
     }
 };
@@ -25,5 +25,6 @@ module.exports = Versioning;
 
 
 //** TEST CODE ****/
-console.log(Versioning.update("major"));
-
+//console.log(Versioning.update("patch"));
+//console.log(Versioning.update("minor"));
+//console.log(Versioning.update("major"));
