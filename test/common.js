fbcead7d1734aaa00d24cb58bdd95d8d96a033e3
sternba <Adam.Sternberg@nordstrom.com>
2017-08-17 07:50:42 -0700

    add sinon, sinon-chai dev deps

--- test/common.js
+++ test/common.js
@@ -6,10 +6,15 @@ global.SRC_HOME = PROJECT_HOME + '/src';
 global.Promise = require('bluebird');
 
 // testing modules
+global.sinon = require('sinon');
+
 global.chai = require("chai");
 global.expect = require("chai").expect;
 global.should = require("chai").should;
 chai.should();
+
+global.sinonChai = require('sinon-chai');
+chai.use(sinonChai);
 
 global.chaiAsPromised = require('chai-as-promised')
 chai.use(chaiAsPromised)
@@ -17,4 +22,3 @@ chai.use(chaiAsPromised)
 global.nock = require('nock')
 
 global.AssertionError = require("chai").AssertionError;
-
